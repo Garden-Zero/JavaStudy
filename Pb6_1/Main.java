fdc54e3ca27caded7478cc80f9d60006c8094c59
Garden_zero <85011588+Garden-Zero@users.noreply.github.com>
2023-08-16 23:59:48 +0900

    Create Main.java
    sovled

---
+++ Pb6_1/Main.java
@@ -0,0 +1,26 @@
+class SutdaCard{
+    int num;
+    boolean isKwang;
+    SutdaCard(){
+        this(1, true);
+    }
+    SutdaCard(int num, boolean isKwang){
+        this.num = num;
+        this.isKwang = isKwang;
+    }
+    String info(){
+        return isKwang ? Integer.toString(num)+"K" : Integer.toString(num);
+    }
+}
+
+public class Main {
+    public static void main(String[] args){
+        SutdaCard card1 = new SutdaCard(3, false);
+        SutdaCard card2 = new SutdaCard();
+
+        System.out.println(card1.info());
+        System.out.println(card2.info());
+
+
+    }
+}
