163cf020e56944cdc6fd98a94370284894213100
Garden_zero <85011588+Garden-Zero@users.noreply.github.com>
2023-09-03 18:19:44 +0900

    solved Pb9_2

---
+++ src/Pb9_2/Pb9_2.java
@@ -0,0 +1,40 @@
+import java.awt.*;
+import java.io.PipedInputStream;
+
+public class Pb9_2 {
+    public static void main(String[] args){
+        Point3D p1 = new Point3D(1, 2, 3);
+        Point3D p2 = new Point3D(1, 2, 3);
+
+        System.out.println(p1);
+        System.out.println(p2);
+        System.out.println(p1==p1);
+        System.out.println(p1.equals(p2));
+    }
+}
+
+class Point3D{
+    int x, y, z;
+
+    Point3D(){
+        this(0, 0, 0);
+    }
+    Point3D(int x, int y, int z){
+        this.x = x;
+        this.y = y;
+        this.z = z;
+    }
+
+    public boolean equals(Object obj){
+        if(obj instanceof Point3D){
+            Point3D p = (Point3D)obj;
+            return x==p.x && y==p.y && z==p.z;
+        }
+        return false;
+    }
+    public String toString(){
+        return "x: "+x+" y: "+y+" z: "+z;
+    }
+
+
+}
